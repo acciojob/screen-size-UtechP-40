3f292c1e6d81df4b11afccf8541a8429e52247fe
Pradeep Pandey <90238672+UtechP-40@users.noreply.github.com>
2024-09-28 02:42:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your JS code here. If required.
+let width = window.innerWidth;
+let height = window.innerHeight;
+// console.log(width,height)
+let head = document.createElement("h1");
+head.innerText = `Width:${width} and Height:${height}`;
+
+document.querySelector("#sizeInfo").append(head)
